4b9fa249e057ac9f7d2e5aa612216ac529d47965
Corban Riley <corbanbrook@gmail.com>
2023-06-19 10:18:09 -0400

    Updating design-system deps

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,9 @@
+import '@0xsequence/design-system/styles.css'
+
 import { ThemeProvider } from '@0xsequence/design-system'
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
-
-import '@0xsequence/design-system/styles.css'
 
 const root = createRoot(document.getElementById('root'))
 
